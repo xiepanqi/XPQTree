f305a0c448f946cfee6360b553287e2eb328f59d
xiepanqi <xiepanqi@xiepanqideMac-mini.local>
2015-06-04 16:31:54 +0800

    Initial Commit

---
+++ XPQTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XPQTree
+//
+//  Created by 谢攀琪 on 15/6/4.
+//  Copyright (c) 2015年 谢攀琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XPQTree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XPQTree
+//
+//  Created by 谢攀琪 on 15/6/4.
+//  Copyright (c) 2015年 谢攀琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
